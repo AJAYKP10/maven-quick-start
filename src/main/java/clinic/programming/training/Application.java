c5736831b515d09c6d956680c8642cb9cc2685e0
Ajay <ajaykpchikku777@gmail.com>
2021-09-13 11:35:11 +0530

    use stringutils and added dependecies to lang3

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,8 +1,15 @@
 package clinic.programming.training;
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
+
+       public int countWords(String words){
+          String[] seperateWords=StringUtils.split(words, ' ');
+return (seperateWords==null)?0:seperateWords.length;
+}
+
     public void greet(){
          List<String> greetings=new ArrayList<>();
 greetings.add("Hello");
@@ -21,5 +28,7 @@ for(String greeting : greetings){
     	System.out.println ("Starting Application");
 	Application app = new Application();
         app.greet();
+        int count=app.countWords("I have four words");
+System.out.println("Word count:"+count); 
     }
 }
